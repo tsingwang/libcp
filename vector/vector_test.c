0335587ba905865625ba0b86e802dd2f4ba103e9
Tsing Wang <tsing.nix@outlook.com>
2024-07-01 20:22:33 +0800

    Add vector string test

--- vector/vector_test.c
+++ vector/vector_test.c
@@ -3,12 +3,16 @@
 
 #include <stdio.h>
 
-static int cmp(const void *a, const void *b) {
+int cmp(const void *a, const void *b) {
     return ( *(int*)a - *(int*)b );
 }
 
-void vector_test(void) {
-    vector_uint *v = vector_new(v);
+int cmpstr(const void *a, const void *b) {
+    return strcmp(*(const char**)a, *(const char**)b);
+}
+
+void vector_test_0(void) {
+    vector_int *v = vector_new(v);
     vector_push_back(v, 3);
     vector_push_back(v, 1);
     vector_push_back(v, 2);
@@ -42,9 +46,26 @@ void vector_test(void) {
     test_cond("vector search", p == NULL);
 
     vector_free(v);
-    test_report();
+}
+
+void vector_test_1(void) {
+    vector_str *v = vector_new(v);
+    vector_push_back(v, "345");
+    vector_push_back(v, "15");
+    vector_push_back(v, "2244");
+    vector_push_back(v, "3");
+    vector_pop_back(v);
+    test_cond("vector push & pop", v->size == 3);
+    test_cond("vector back", memcmp(vector_back(v), "2244\0", 5) == 0);
+    test_cond("vector sorted, need CHECK by your eyes", 1);
+    vector_sort(v, cmpstr);
+    for (int i = 0; i < v->size; i++)
+        printf("%s ", v->data[i]);
+    vector_free(v);
 }
 
 int main(void) {
-    vector_test();
+    vector_test_0();
+    vector_test_1();
+    test_report();
 }
